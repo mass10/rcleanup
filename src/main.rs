77a02bd4e87fb202e11dd000676dde49c725dc75
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-02-28 10:23:22 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
 mod application;
 
+fn usage() {
+	println!("USAGE:");
+	println!("    rcleanup {{location}}");
+	println!();
+}
+
 /// アプリケーションのエントリーポイント
 fn main() {
-	// アプリケーションのインスタンスを初期化します。
-	let app = application::Application::new();
-	if app.is_err() {
-		println!("[ERROR] {}", app.err().unwrap());
+	// コマンドライン引数
+	let args: Vec<String> = std::env::args().skip(1).collect();
+	if args.len() == 0 {
+		usage();
 		return;
 	}
-	let app = app.unwrap();
 
-	// アプリケーションを実行します。
-	let result = app.run();
+	// アプリケーションのインスタンスを初期化します。
+	let result = application::Application::new();
 	if result.is_err() {
 		println!("[ERROR] {}", result.err().unwrap());
+		std::thread::sleep(std::time::Duration::from_millis(2500));
 		return;
 	}
+	let application = result.unwrap();
+
+	// アプリケーションを実行します。
+	let result = application.run(&args);
+	if result.is_err() {
+		println!("[ERROR] {}", result.err().unwrap());
+		std::thread::sleep(std::time::Duration::from_millis(2500));
+		return;
+	}
+
+	std::thread::sleep(std::time::Duration::from_millis(2500));
 }
